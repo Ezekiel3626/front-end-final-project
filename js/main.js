7a321b7d4a346b1d59ec9381a6f4b53037766b35
Ezekiel Lim <ezekiel.c.lim@gmail.com>
2017-03-16 19:25:29 -0400

    updated html text

--- js/main.js
+++ js/main.js
@@ -4,4 +4,38 @@ $(document).on('ready', function(){
 
 	if (5 < 10) {   console.log('But you will definitely see this'); }
 
+	// attach click event to js-menu
+	$('.js-menu').on('click', function(){
+		// do things
+		$('.nav-main').slideToggle();
+	});
+
+	$(window).on('resize', function(){
+		var w = $(window).width();
+		console.log('I am resizing!!!' + w);
+
+	// write an if statement, if width > 420, do something
+	if(w > 480 && $('.nav-main').is('[style]')){
+		console.log('removed style');
+		$('.nav-main').removeAttr('style');
+	}
+
+	});
+	// attach click event to main-nav item
+	$('.nav-main a').on('click', function(event){
+		console.log(event);
+		event.preventDefault();
+
+		$('.nav-main a').removeClass('is-current');
+		// this refers to the DOM element being operated on
+		$(this).addClass('is-current');
+		// $(this) is just 'this', wrapped in a jQuery selector
+
+ 		console.log($(this).attr('href'));
+ 		// store $(this).attr('href') in a variable
+ 		var pageToScrollTo = $(this).attr('href');
+ 		// use that variable in our $.scrollTo() function
+ 		$(window).scrollTo(pageToScrollTo, 5000);
+	});
+
 });

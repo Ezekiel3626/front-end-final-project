6f4c9d065a112446830f1c3df53da796e5eb1b45
Ezekiel Lim <ezekiel.c.lim@gmail.com>
2017-03-16 19:11:41 -0400

    added js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).on('ready', function(){
+
+	if (5 > 10) {   console.log("You'll never see this in the console because 5 is not greater than 10"); }
+
+	if (5 < 10) {   console.log('But you will definitely see this'); }
+	
+}

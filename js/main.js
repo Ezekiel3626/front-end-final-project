dad2513b4cab32fba078ec6f0f405d10075b42f0
Ezekiel Lim <ezekiel.c.lim@gmail.com>
2017-03-16 19:14:50 -0400

    added classes to p

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ $(document).on('ready', function(){
 	if (5 > 10) {   console.log("You'll never see this in the console because 5 is not greater than 10"); }
 
 	if (5 < 10) {   console.log('But you will definitely see this'); }
-	
-}
+
+});

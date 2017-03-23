f839832a1b8fdeba18c2f34ebc4d434aa9f871f6
Ezekiel Lim <ezekiel.c.lim@gmail.com>
2017-03-23 18:34:32 -0400

    added text and images

--- js/main.js
+++ js/main.js
@@ -38,4 +38,25 @@ $(document).on('ready', function(){
  		$(window).scrollTo(pageToScrollTo, 5000);
 	});
 
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
+	alert("Booooooo Phillies!");
+	
+});
+
 });
